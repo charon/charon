54eccad0371d67e913efc823c9555e03a13a4995
Mitar <mitar.git@tnode.com>
2023-11-22 12:34:07 +0100

    Add Vite.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/* global module */
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    "vue/setup-compiler-macros": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/typescript/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "none",
+      },
+    ],
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
