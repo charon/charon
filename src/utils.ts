bd3f9dda45e9bce0ea170748651429fdd26d8cb7
Mitar <mitar.git@tnode.com>
2023-12-11 23:30:28 +0100

    Typing.

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import type { AuthFlowResponse } from "@/types"
 export function locationRedirect(response: AuthFlowResponse): boolean {
   // We do not use Vue Router to force a server-side request which might return updated cookies
   // or redirect on its own somewhere because of new (or lack thereof) cookies.
-  if (response.location) {
+  if ("location" in response) {
     if (response.location.replace) {
       window.location.replace(response.location.url)
     } else {
