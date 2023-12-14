f046b8e9fd7ae4ddf98f8114a3d8e7c1b986910d
Mitar <mitar.git@tnode.com>
2023-12-14 11:07:07 +0100

    Style.

--- src/context.ts
+++ src/context.ts
@@ -2,8 +2,8 @@ import type { SiteContext } from "@/types"
 
 // TODO: Use import with import assertion?
 //       See: https://github.com/vitejs/vite/issues/4934
-// redirect: "error" cannot be set because then preload is not matched in Firefox. We have
-// a hard-coded URL here instead of resolving it from the router to simplify imports order.
+// redirect: "error" cannot be set because then preload is not matched in Firefox.
+// We have a hard-coded URL here instead of resolving it from the router to simplify imports order.
 export default (await fetch("/api", {
   method: "GET",
   // Mode and credentials match crossorigin=anonymous in link preload header.
