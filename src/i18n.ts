5bec3c232f489dca7990e09870dc66ee99d43f18
Mitar <mitar.git@tnode.com>
2025-08-22 12:34:05 +0200

    Simplify logic.

--- src/i18n.ts
+++ src/i18n.ts
@@ -18,19 +18,16 @@ export const i18n = createI18n<[MessageSchema], "en" | "sl">({
   messages,
   pluralRules: {
     sl: (choice: number, choicesLength: number) => {
-      if (choice === 0) {
+      if (choice % 100 === 1) {
         return 0
       }
-      if (choice % 100 === 1) {
+      if (choice % 100 === 2) {
         return 1
       }
-      if (choice % 100 === 2) {
+      if (choice % 100 === 3 || choice % 100 === 4) {
         return 2
       }
-      if (choice % 100 === 3 || choice % 100 === 4) {
-        return 3
-      }
-      return 4
+      return 3
     },
   },
 })
