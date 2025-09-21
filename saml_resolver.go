90a0f73c0f65a3bfd2481dbf98be35a812d3d16f
Mitar <mitar.git@tnode.com>
2025-09-21 10:02:28 +0200

    Move.

--- saml_resolver.go
+++ saml_resolver.go
@@ -7,6 +7,30 @@ import (
 	"gitlab.com/tozd/go/errors"
 )
 
+type SAMLAttributeMapping struct {
+	CredentialIDAttribute string            `yaml:"credentialIdAttribute"`
+	Mappings              map[string]string `yaml:"mappings"`
+}
+
+func getSIPASSAttributeMapping() SAMLAttributeMapping {
+	return SAMLAttributeMapping{
+		CredentialIDAttribute: "1.3.6.1.4.1.44044.1.1.3.2", // VATNumber.
+		Mappings: map[string]string{
+			"1.3.6.1.4.1.44044.1.1.3.1":  "token", // SICAS Token - unique identifier.
+			"1.3.6.1.4.1.44044.1.1.3.2":  "authMethod",
+			"1.3.6.1.4.1.44044.1.1.3.3":  "authMechanism",
+			"1.3.6.1.4.1.44044.1.1.3.10": "language",
+		},
+	}
+}
+
+func getDefaultAttributeMapping() SAMLAttributeMapping {
+	return SAMLAttributeMapping{
+		CredentialIDAttribute: "NameID",
+		Mappings:              map[string]string{},
+	}
+}
+
 type SAMLCredentialResolver interface {
 	ResolveCredentialID(assertionInfo *saml2.AssertionInfo) (string, errors.E)
 	GetAttributeMapping() SAMLAttributeMapping
--- site.go
+++ site.go
@@ -11,11 +11,6 @@ type Build struct {
 	Revision       string `json:"revision,omitempty"`
 }
 
-type SAMLAttributeMapping struct {
-	CredentialIDAttribute string            `yaml:"credentialIdAttribute"`
-	Mappings              map[string]string `yaml:"mappings"`
-}
-
 type SiteProvider struct {
 	Key  Provider               `json:"key"`
 	Name string                 `json:"name"`
@@ -41,23 +36,4 @@ type Site struct {
 
 	Build     *Build         `json:"build,omitempty"`
 	Providers []SiteProvider `json:"providers"`
-}
-
-func getSIPASSAttributeMapping() SAMLAttributeMapping {
-	return SAMLAttributeMapping{
-		CredentialIDAttribute: "1.3.6.1.4.1.44044.1.1.3.2", // VATNumber.
-		Mappings: map[string]string{
-			"1.3.6.1.4.1.44044.1.1.3.1":  "token", // SICAS Token - unique identifier.
-			"1.3.6.1.4.1.44044.1.1.3.2":  "authMethod",
-			"1.3.6.1.4.1.44044.1.1.3.3":  "authMechanism",
-			"1.3.6.1.4.1.44044.1.1.3.10": "language",
-		},
-	}
-}
-
-func getDefaultAttributeMapping() SAMLAttributeMapping {
-	return SAMLAttributeMapping{
-		CredentialIDAttribute: "NameID",
-		Mappings:              map[string]string{},
-	}
 }
