a87f322d8b3a4353112109dce5d17d2d82d49116
Mitar <mitar.git@tnode.com>
2025-09-23 00:32:36 +0200

    Add retrieveAssertionInfoWithResponse.

---
+++ saml_response.go
@@ -0,0 +1,75 @@
+package charon
+
+import (
+	saml2 "github.com/russellhaering/gosaml2"
+	"github.com/russellhaering/gosaml2/types"
+)
+
+// retrieveAssertionInfoWithResponse is the same as saml2.RetrieveAssertionInfo, but
+// returns the response as well.
+//
+// See: https://github.com/russellhaering/gosaml2/issues/235
+// See: https://github.com/russellhaering/gosaml2/pull/236
+func retrieveAssertionInfoWithResponse(sp *saml2.SAMLServiceProvider, encodedResponse string) (*saml2.AssertionInfo, *types.Response, error) {
+	assertionInfo := &saml2.AssertionInfo{
+		Values: make(saml2.Values),
+	}
+
+	response, err := sp.ValidateEncodedResponse(encodedResponse)
+	if err != nil {
+		return nil, nil, saml2.ErrVerification{Cause: err}
+	}
+
+	// TODO: Support multiple assertions
+	if len(response.Assertions) == 0 {
+		return nil, nil, saml2.ErrMissingAssertion
+	}
+
+	assertion := response.Assertions[0]
+	assertionInfo.Assertions = response.Assertions
+	assertionInfo.ResponseSignatureValidated = response.SignatureValidated
+
+	warningInfo, err := sp.VerifyAssertionConditions(&assertion)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	//Get the NameID
+	subject := assertion.Subject
+	if subject == nil {
+		return nil, nil, saml2.ErrMissingElement{Tag: saml2.SubjectTag}
+	}
+
+	nameID := subject.NameID
+	if nameID == nil {
+		return nil, nil, saml2.ErrMissingElement{Tag: saml2.NameIdTag}
+	}
+
+	assertionInfo.NameID = nameID.Value
+
+	//Get the actual assertion attributes
+	attributeStatement := assertion.AttributeStatement
+	if attributeStatement == nil && !sp.AllowMissingAttributes {
+		return nil, nil, saml2.ErrMissingElement{Tag: saml2.AttributeStatementTag}
+	}
+
+	if attributeStatement != nil {
+		for _, attribute := range attributeStatement.Attributes {
+			assertionInfo.Values[attribute.Name] = attribute
+		}
+	}
+
+	if assertion.AuthnStatement != nil {
+		if assertion.AuthnStatement.AuthnInstant != nil {
+			assertionInfo.AuthnInstant = assertion.AuthnStatement.AuthnInstant
+		}
+		if assertion.AuthnStatement.SessionNotOnOrAfter != nil {
+			assertionInfo.SessionNotOnOrAfter = assertion.AuthnStatement.SessionNotOnOrAfter
+		}
+
+		assertionInfo.SessionIndex = assertion.AuthnStatement.SessionIndex
+	}
+
+	assertionInfo.WarningInfo = warningInfo
+	return assertionInfo, response, nil
+}
