20f6f1e95cf2eb336f713d360c17ea158e75458a
KukovecRok <kukov.rok@gmail.com>
2026-01-16 08:35:16 +0100

    fmt

--- src/views/OrganizationUserRoles.vue
+++ src/views/OrganizationUserRoles.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type {IdentityForAdmin, Metadata, Organization, Role} from "@/types"
+import type { IdentityForAdmin, Metadata, Organization, Role } from "@/types"
 
 import { onBeforeMount, onBeforeUnmount, ref, watch } from "vue"
 import { useI18n } from "vue-i18n"
