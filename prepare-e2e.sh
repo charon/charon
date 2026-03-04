14033fdfeb3e0b5fae403a6a042e98e81b342568
David Marn <david.marn@gmail.com>
2026-03-04 17:51:20 +0100

    Expands visual testing comparisons.

--- prepare-e2e.sh
+++ prepare-e2e.sh
@@ -17,9 +17,17 @@ if [ -n "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
     echo "Target branch has no open MR, downloading screenshots from branch artifacts."
     wget -O artifacts.zip "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/artifacts/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}/download?job=test_e2e"
   fi
-
-  unzip -o artifacts.zip -x playwright-report/\* -x test-results/\* -x coverage.html -x coverage.xml -x coverage-frontend/\* -x a11y-report/\*
 else
-  echo "Branch or tag pipeline (not MR), update screenshots."
-  export UPDATE_SCREENSHOTS=changed
+  if [ -n "$CI_COMMIT_TAG" ] || [ "$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH" ]; then
+    echo "Tag or main branch pipeline, update screenshots."
+    export UPDATE_SCREENSHOTS=changed
+  else
+    echo "Branch pipeline for $CI_COMMIT_BRANCH, downloading screenshots from main branch ($CI_DEFAULT_BRANCH)."
+    wget -O artifacts.zip "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/jobs/artifacts/${CI_DEFAULT_BRANCH}/download?job=test_e2e"
+  fi
+fi
+
+# Unzip artifacts if downloaded.
+if [ -f artifacts.zip ]; then
+  unzip -o artifacts.zip -x playwright-report/\* -x test-results/\* -x coverage.html -x coverage.xml -x coverage-frontend/\* -x a11y-report/\*
 fi
